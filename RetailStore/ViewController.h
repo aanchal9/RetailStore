46b2454d871a748324926a47ce6cdba6c31ab45a
Aanchal Arora <aanchal.arora175@gmail.com>
2015-07-29 12:12:31 +0530

    Initial Commit

---
+++ RetailStore/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RetailStore
+//
+//  Created by Aanchal Arora on 29/07/15.
+//  Copyright (c) 2015 Aanchal Arora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RetailStore/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RetailStore
+//
+//  Created by Aanchal Arora on 29/07/15.
+//  Copyright (c) 2015 Aanchal Arora. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
